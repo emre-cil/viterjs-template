5862eb16f302d1012df3b1a90aefc1e340dc759d
emre-cil <info.emrecil@gmail.com>
2022-09-11 16:22:24 +0300

    spinner updated to mui

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,22 +1,28 @@
-import styled from 'styled-components';
+import { Box, useTheme } from '@mui/material';
+const Spinner = () => {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        m: '2rem auto',
+        width: '2.5rem',
+        height: '2.5rem',
+        borderRadius: '50%',
+        animation: 'spinner 0.5s linear infinite',
+        border: `5px solid ${theme.palette.grey[300]}`,
+        borderTop: `5px solid ${theme.palette.primary.main}`,
 
-const Spinner = styled.div`
-  width: 2.5rem;
-  height: 2.5rem;
-  border-radius: 50%;
-  border: 5px solid var(--lightGrey);
-  border-top: 5px solid var(--medGrey);
-  animation: spinner 0.5s linear infinite;
-  margin: 2rem auto;
-  margin-bottom: 0;
-  @keyframes spinner {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
-  }
-`;
+        ' @keyframes spinner ': {
+          '0%': {
+            transform: 'rotate(0deg)',
+          },
+          '100%': {
+            transform: 'rotate(360deg)',
+          },
+        },
+      }}
+    ></Box>
+  );
+};
 
 export default Spinner;
