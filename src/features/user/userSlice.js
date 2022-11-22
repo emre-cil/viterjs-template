1d42ee2c642d7137981d5e5c99b86a4f116ded62
emre-cil <info.emrecil@gmail.com>
2022-11-22 20:40:32 +0300

    update: change mode now stored

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,7 +6,11 @@ const initialState = {
     FirstName: '',
     LastName: '',
   },
-  mode: window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light',
+  mode: localStorage.getItem('mode')
+    ? localStorage.getItem('mode')
+    : window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'dark'
+    : 'light',
 };
 
 export const userSlice = createSlice({
@@ -28,8 +32,10 @@ export const userSlice = createSlice({
     changeMode: (state) => {
       if (state.mode === 'light') {
         state.mode = 'dark';
+        localStorage.setItem('mode', 'dark');
       } else {
         state.mode = 'light';
+        localStorage.setItem('mode', 'light');
       }
     },
   },
