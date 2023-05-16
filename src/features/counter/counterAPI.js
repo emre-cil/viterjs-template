0a11ac9c565ab79d07f462adb160a2ce463d62f0
emre cil <info.emrecil@gmail.com>
2023-05-16 10:09:16 +0300

    chore: bump version 4.3.6

--- src/features/counter/counterAPI.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-promise-executor-return */
-// A mock function to mimic making an async request for data
-export default function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500),
-  );
-}
